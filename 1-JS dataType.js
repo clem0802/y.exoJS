ba33d66e154d41e4b7c2208906e84c77033f3f1b
clem0802 <hhcc0802@gmail.com>
2022-05-26 22:37:05 +0200

    types, operators

--- 1-JS dataType.js
+++ 1-JS dataType.js
@@ -1,5 +1,5 @@
+// check output via F12
 // What are the JavaScript data types?
-
 Number
 String
 Boolean
@@ -7,3 +7,29 @@ Object
 Symbol
 Undefined
 Null
+
+/*------------------------------*/
+let isBatteryOn = true;
+let isSwitchOn = true;
+console.log(isBatteryOn && isSwitchOn); // true
+let result = isBatteryOn && isSwitchOn;
+console.log(result); // true 
+
+/*------------------------------*/
+let isBatteryOn = true;
+let isPowerOn = false;
+console.log(!(isBatteryOn && isPowerOn)); // true
+
+/*------------------------------*/
+let condition1 = true;
+let condition2 = false;
+let logicalExpression = condition1 || condition2;
+console.log(logicalExpression); // true
+console.log(!logicalExpression || logicalExpression); // true
+
+/*------------------------------*/
+let cloudStorage = 4;
+let systemStorage = 8;
+let fileSize = 5;
+let result = (cloudStorage > fileSize) || (systemStorage > fileSize);
+console.log(`Saved? : ${result}`); // Saved? : true
