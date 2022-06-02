fe5957a231a72d341463b9a1731634724083c46f
clem0802 <hhcc0802@gmail.com>
2022-06-02 02:06:08 +0200

    2 min num

---
+++ 29-2minNum.js
@@ -0,0 +1,9 @@
+// find two smallest numbers in a number array
+// ASC & DESC orders
+const array = [1, 10, 2, 7, 5,3, 4];
+const ascending = array.slice().sort((a, b) => a - b).slice(0, 2);
+const descending = array.slice().sort((a, b) => b - a).slice(-2);
+
+console.log(array.join()); // to show it isn't changed
+console.log(ascending.join());
+console.log(descending.join());
