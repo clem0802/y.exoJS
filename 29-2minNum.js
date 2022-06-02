a70d8c3e72ea3aa28d7abac70a15be94d2593815
clem0802 <hhcc0802@gmail.com>
2022-06-02 02:08:26 +0200

    --

--- 29-2minNum.js
+++ 29-2minNum.js
@@ -1,3 +1,5 @@
+/*---------------------------------------------*/ 
+// Find the 2 smallest numbers
 // find two smallest numbers in a number array
 // ASC & DESC orders
 const array = [1, 10, 2, 7, 5,3, 4];
@@ -7,3 +9,10 @@ const descending = array.slice().sort((a, b) => b - a).slice(-2);
 console.log(array.join()); // to show it isn't changed
 console.log(ascending.join());
 console.log(descending.join());
+
+
+/*---------------------------------------------*/ 
+// Sum the 2 smallest numbers
+function sumTwoSmallestNumbers(numbers) {  
+    //Code here
+}
